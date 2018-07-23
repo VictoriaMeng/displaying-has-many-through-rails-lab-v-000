ca33050878783330da8bf51fc705da06b100cc1c
Victoria Meng <nights1ststar@protonmail.com>
2018-07-23 00:07:54 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180723000726) do
+
+  create_table "doctors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "department"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
